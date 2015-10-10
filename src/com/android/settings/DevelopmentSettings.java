7e9d74ac2c17f0d2a3e38ca343d84a23dc426921
George G <kreach3r@users.noreply.github.com>
2015-10-10 18:25:33 +0300

    Enable Development Settings for everyone, too!
    User and userdebug builds were jealous.
    
    Signed-off-by: Corey Edwards <ensabahnur16@gmail.com>

--- src/com/android/settings/DevelopmentSettings.java
+++ src/com/android/settings/DevelopmentSettings.java
@@ -1914,7 +1914,7 @@ public class DevelopmentSettings extends SettingsPreferenceFragment
                     return context.getSharedPreferences(DevelopmentSettings.PREF_FILE,
                             Context.MODE_PRIVATE).getBoolean(
                                     DevelopmentSettings.PREF_SHOW,
-                                    android.os.Build.TYPE.equals("eng"));
+                                    true);
                 }
 
                 @Override
--- src/com/android/settings/SettingsActivity.java
+++ src/com/android/settings/SettingsActivity.java
@@ -1227,7 +1227,7 @@ public class SettingsActivity extends Activity
     private void updateTilesList(List<DashboardCategory> target) {
         final boolean showDev = mDevelopmentPreferences.getBoolean(
                 DevelopmentSettings.PREF_SHOW,
-                android.os.Build.TYPE.equals("eng"));
+                true);
 
         final UserManager um = (UserManager) getSystemService(Context.USER_SERVICE);
 
