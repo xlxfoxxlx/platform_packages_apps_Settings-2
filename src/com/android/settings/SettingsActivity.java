e2ad46b2278a53f95a40bb49cd892b1c3a1f1413
Altaf-Mahdi <altaf.mahdi@gmail.com>
2015-12-09 14:47:57 +0000

    Expanded Desktop tile longpress action
    Change-Id: If6f00736d07580f518cbbcd1ec1a377174d25728
    
    Conflicts:
    	src/com/android/settings/Settings.java
    	src/com/android/settings/SettingsActivity.java

--- src/com/android/settings/SettingsActivity.java
+++ src/com/android/settings/SettingsActivity.java
@@ -72,6 +72,7 @@ import com.android.settings.accounts.AccountSettings;
 import com.android.settings.accounts.AccountSyncSettings;
 import com.android.settings.aosip.OwlsNestSettings;
 import com.android.settings.applications.DrawOverlayDetails;
+import com.android.settings.applications.ExpandedDesktopPreferenceFragment;
 import com.android.settings.applications.InstalledAppDetails;
 import com.android.settings.applications.ManageApplications;
 import com.android.settings.applications.ManageAssist;
@@ -357,7 +358,8 @@ public class SettingsActivity extends Activity
             ProcessStatsSummary.class.getName(),
             DrawOverlayDetails.class.getName(),
             WriteSettingsDetails.class.getName(),
-            OwlsNestSettings.class.getName()
+            OwlsNestSettings.class.getName(),
+            ExpandedDesktopPreferenceFragment.class.getName()
     };
 
 
