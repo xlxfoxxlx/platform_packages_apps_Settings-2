06388e7cec0b6540df377e3bfe421197dc24b5e1
droyd <mikew8978@gmail.com>
2015-12-23 00:12:14 -0500

    Squash statusbar options
    Clock OC https://github.com/XXMrHyde/android_packages_apps_Settings/commit/a215e0ef014e5a730e929f57f3dfeea26fcf7e2d
    carrier label OC https://github.com/XXMrHyde/android_packages_apps_Settings/commit/f503cd895656234c2460448a04b91614e84539c2
    Network traffic OC https://github.com/XXMrHyde/android_packages_apps_Settings/commit/9b064c40641335c095d03d8535b7ff95d597c1f4
    Adapted to our sliders with minimal adjustments credit XXMrHyde
    
    Conflicts:
    	res/values/custom_arrays.xml
    	res/values/custom_strings.xml
    	src/com/android/settings/aosip/statusbar/StatusbarHolder.java

--- src/com/android/settings/aosip/statusbar/StatusbarHolder.java
+++ src/com/android/settings/aosip/statusbar/StatusbarHolder.java
@@ -52,6 +52,7 @@ import com.android.settings.SettingsPreferenceFragment;
 import com.android.internal.logging.MetricsLogger;
 
 import com.android.settings.aosip.statusbar.ClockCategory;
+import com.android.settings.aosip.statusbar.CarrierlabelCategory;
 import com.android.settings.aosip.statusbar.BatteryCategory;
 import com.android.settings.aosip.statusbar.IconsCategory;
 import com.android.settings.aosip.PagerSlidingTabStrip;
@@ -111,6 +112,8 @@ public class StatusbarHolder extends SettingsPreferenceFragment {
             frags[0] = new ClockCategory();
             frags[1] = new BatteryCategory();
             frags[2] = new IconsCategory();
+            frags[3] = new CarrierlabelCategory();
+            frags[4] = new TrafficCategory();
         }
 
         @Override
@@ -134,7 +137,9 @@ public class StatusbarHolder extends SettingsPreferenceFragment {
         titleString = new String[]{
                     getString(R.string.clock_category),
                     getString(R.string.battery_category),
-                    getString(R.string.icon_category)};
+                    getString(R.string.icon_category),
+                    getString(R.string.carrier_category),
+                    getString(R.string.traffic_category)};
         return titleString;
     }
 }
