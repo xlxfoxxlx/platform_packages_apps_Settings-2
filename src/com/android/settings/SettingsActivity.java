voidz777-pure/android_packages_apps_Settings

Add back blacklist settings. [Squashed]
Cm originally had this squashed, but with the mess that is cm-13 even theyre squashed became unsquashed and you had to dig in like 10 commits that had nothing to do with blacklist to find all the code. thats what happens when one person merges fwbase and another setting.. c'mon man organize your shit!

src/com/android/settings/SettingsActivity.java
@@ -79,6 +79,7 @@ import com.android.settings.applications.ProcessStatsSummary;
 import com.android.settings.applications.ProcessStatsUi;
 import com.android.settings.applications.UsageAccessDetails;
 import com.android.settings.applications.WriteSettingsDetails;
+import com.android.settings.blacklist.BlacklistSettings;
 import com.android.settings.bluetooth.BluetoothSettings;
 import com.android.settings.nexus.DisplayRotation;
 import com.android.settings.dashboard.DashboardCategory;
@@ -364,7 +365,8 @@ public class SettingsActivity extends Activity
             BatteryLightSettings.class.getName(),
             NotificationLightSettings.class.getName(),
             ExpandedDesktopPreferenceFragment.class.getName(),
-            DisplayRotation.class.getName()
+            DisplayRotation.class.getName(),
+            BlacklistSettings.class.getName()
     };
 
 

xlxfoxxlx/platform_packages_apps_Settings-2

Change-Id: I45d6c90bd584d65c83504a0fdca96f69f4ee7d94
Conflicts:
	res/values/du_arrays.xml
	res/values/du_colors.xml
	res/values/du_strings.xml
	src/com/android/settings/Settings.java
	src/com/android/settings/SettingsActivity.java
	AndroidManifest.xml
	res/values/cm_colors.xml
	res/values/cm_styles.xml
	res/values/custom_arrays.xml
	res/values/custom_strings.xml

src/com/android/settings/SettingsActivity.java
@@ -79,6 +79,7 @@ import com.android.settings.applications.ProcessStatsSummary;
 import com.android.settings.applications.ProcessStatsUi;
 import com.android.settings.applications.UsageAccessDetails;
 import com.android.settings.applications.WriteSettingsDetails;
+import com.android.settings.blacklist.BlacklistSettings;
 import com.android.settings.bluetooth.BluetoothSettings;
 import com.android.settings.dashboard.DashboardCategory;
 import com.android.settings.dashboard.DashboardSummary;
@@ -356,7 +357,8 @@ public class SettingsActivity extends Activity
             ProcessStatsSummary.class.getName(),
             DrawOverlayDetails.class.getName(),
             WriteSettingsDetails.class.getName(),
-            OwlsNestSettings.class.getName()
+            OwlsNestSettings.class.getName(),
+            BlacklistSettings.class.getName()
     };
 
 